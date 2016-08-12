a9bc76f308b250c645afec1d3b5ca854ec004cef
WuJianbo <t1ger@zhihu.com>
2016-08-12 10:23:53 +0800

    fixed import error

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import vForm from './source/ValidateableForm.react.jsx';
+'use strict';
 
-export default vForm;
-module.exports = vForm;
+exports.__esModule = true;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _ReactFormValidator = require('./source/ValidateableForm.react.js');
+
+var _ReactFormValidator2 = _interopRequireDefault(_ReactFormValidator);
+
+exports['default'] = _ReactFormValidator2['default'];
+module.exports = exports['default'];
